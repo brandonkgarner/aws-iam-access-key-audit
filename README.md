bf6ac1b554cad86c51bacd12331779d582d277a2
Brandon Garner <brandon.k.garner@gmail.com>
2024-10-15 13:01:49 -0400

    Add status flagging and other much needed improvements

--- README.md
+++ README.md
@@ -9,39 +9,58 @@ AWS profiles should already be configured on the machine.
 ## Usage
 
 ### Options
-    -p, --profiles   Comma-separated list of AWS profiles (default: ( default ))"
-    -r, --region     AWS region (default: us-east-1)"
-    --raw            Disable all formatting"
-    --raw-with-old   Disable all formatting except signify old"
-    -h, --help       Display this help message"
+    -p, --profiles                    Comma-separated list of AWS profiles (default: '${OUR_AWS_PROFILES[*]}')"
+    -r, --region                      AWS region (default: '$OUR_AWS_REGION')"
+    --pretty                          Enable pretty formatting"
+    --ignore-inactive                 Ignore inactive access keys"
+    --show-users-without-keys         Show users without access keys"
+    --disable-age-flags               Disable old key age flags"
+    --disable-status-flags            Disable active/inactive key flags"
+    --disable-all-flags               Disable both age and status flags"
+    -h, --help                        Display this help message"
 
 ### Examples
 - `./iam_keys_audit.sh -p dev,qa`
-
-    ![key_audit_pretty.png](img/key_audit_pretty.png)
-
-- `./iam_keys_audit.sh -p dev,qa --raw-with-old`
 
     ```
     dev (012345678901)
         dev-user-1
             AKIAXXXXXXXXXXXXXXXX: 2024-10-08
         dev-user-2
-            AKIAXXXXXXXXXXXXXXXX: 2024-10-08
+            AKIAXXXXXXXXXXXXXXXX: 2022-10-01 (I) *
     qa (012345678902)
         qa-user-1
             AKIAXXXXXXXXXXXXXXXX: None *
     ```
 
-- `./iam_keys_audit.sh -p dev,qa --raw`
+- `./iam_keys_audit.sh -p dev,qa --ignore-inactive`
+
+    ```
+    dev (012345678901)
+        dev-user-1
+            AKIAXXXXXXXXXXXXXXXX: 2024-10-08
+    qa (012345678902)
+        qa-user-1
+            AKIAXXXXXXXXXXXXXXXX: None *
+    ```
+  
+- `./iam_keys_audit.sh -p dev,qa --disable-all-flags`
 
     ```
     dev (012345678901)
         dev-user-1
             AKIAXXXXXXXXXXXXXXXX: 2024-10-08
         dev-user-2
-            AKIAXXXXXXXXXXXXXXXX: 2024-10-08
+            AKIAXXXXXXXXXXXXXXXX: 2022-10-01
     qa (012345678902)
         qa-user-1
             AKIAXXXXXXXXXXXXXXXX: None
     ```
+  
+- `./iam_keys_audit.sh -p dev,qa --pretty`
+
+  ![key_audit_pretty.png](img/key_audit_pretty.png)
+
+- `./iam_keys_audit.sh -p dev,qa --pretty --ignore-inactive --disable-status-flags`
+
+  ![key_audit_pretty_filtered.png](img/key_audit_pretty_filtered.png)
--- iam_keys_audit.sh
+++ iam_keys_audit.sh
@@ -1,35 +1,43 @@
 #!/bin/zsh
 
-IGNORE_USERS_WITHOUT_KEYS=true
 DENOTE_OLD=true
+IGNORE_INACTIVE_KEYS=false
+IGNORE_USERS_WITHOUT_KEYS=true
+DISABLE_AGE_FLAGS=false
+DISABLE_STATUS_FLAGS=false
 OLDER_THAN_DATE=$(date +"%Y")  # i.e. "2024"
 
 # Formatting
-SPACING='    '
+PRETTY=false  # Default to raw mode
 RAW_OLD_SYMBOL='*'
-PRETTY=true
-RED='\033[0;31m'
-NC='\033[0m'  # No Color
+RAW_INACTIVE_SYMBOL='(I)'
+SPACING='    '
+
 BOLD='\033[1m'
+GREEN='\033[0;32m'
+NC='\033[0m'  # No Color
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
 UNDERLINE='\033[4m'
 
 # Default values
-DEFAULT_AWS_REGION='us-east-1'
-DEFAULT_AWS_PROFILES=( default )
-
-OUR_AWS_REGION="$DEFAULT_AWS_REGION"
-OUR_AWS_PROFILES=("${DEFAULT_AWS_PROFILES[@]}")
+OUR_AWS_REGION='us-east-1'
+OUR_AWS_PROFILES=( default )
 
 # Display help function
 usage() {
     echo "Usage: $0 [options]"
     echo
     echo "Options:"
-    echo "  -p, --profiles   Comma-separated list of AWS profiles (default: predefined profiles: ${DEFAULT_AWS_PROFILES[*]})"
-    echo "  -r, --region     AWS region (default: $DEFAULT_AWS_REGION)"
-    echo "  --raw            Disable all formatting"
-    echo "  --raw-with-old   Disable all formatting except signify old"
-    echo "  -h, --help       Display this help message"
+    echo "  -p, --profiles                    Comma-separated list of AWS profiles (default: '${OUR_AWS_PROFILES[*]}')"
+    echo "  -r, --region                      AWS region (default: '$OUR_AWS_REGION')"
+    echo "  --pretty                          Enable pretty formatting"
+    echo "  --ignore-inactive                 Ignore inactive access keys"
+    echo "  --show-users-without-keys         Show users without access keys"
+    echo "  --disable-age-flags               Disable old key age flags"
+    echo "  --disable-status-flags            Disable active/inactive key flags"
+    echo "  --disable-all-flags               Disable both age and status flags"
+    echo "  -h, --help                        Display this help message"
     echo
 }
 
@@ -42,8 +50,12 @@ while [[ "$#" -gt 0 ]]; do
             shift
             ;;
         -r|--region) OUR_AWS_REGION="$2"; shift ;;
-        --raw) PRETTY=false; DENOTE_OLD=false ;;
-        --raw-with-old) PRETTY=false ;;
+        --pretty) PRETTY=true ;;
+        --ignore-inactive) IGNORE_INACTIVE_KEYS=true ;;
+        --show-users-without-keys) IGNORE_USERS_WITHOUT_KEYS=false ;;
+        --disable-age-flags) DISABLE_AGE_FLAGS=true ;;
+        --disable-status-flags) DISABLE_STATUS_FLAGS=true ;;
+        --disable-all-flags) DISABLE_AGE_FLAGS=true; DISABLE_STATUS_FLAGS=true ;;
         -h|--help) usage; exit 0 ;;
         *) echo "Unknown parameter: $1"; usage; exit 1 ;;
     esac
@@ -56,11 +68,11 @@ get_users() {
     aws iam list-users --profile "$profile" --region "$OUR_AWS_REGION" --no-cli-pager --query 'Users[*].UserName' --output text
 }
 
-# Function to get access keys for a user
+# Function to get access keys for a user, including status
 get_access_keys() {
     local user=$1
     local profile=$2
-    aws iam list-access-keys --user "$user" --profile "$profile" --region "$OUR_AWS_REGION" --no-cli-pager --query 'AccessKeyMetadata[*].AccessKeyId' --output text
+    aws iam list-access-keys --user "$user" --profile "$profile" --region "$OUR_AWS_REGION" --no-cli-pager --query 'AccessKeyMetadata[*].[AccessKeyId,Status]' --output text
 }
 
 # Function to get the last used date of an access key
@@ -68,6 +80,10 @@ get_last_used_date() {
     local key=$1
     local profile=$2
     aws iam get-access-key-last-used --access-key-id "$key" --profile "$profile" --region "$OUR_AWS_REGION" --no-cli-pager --query 'AccessKeyLastUsed.LastUsedDate' --output text | cut -d'T' -f1
+}
+
+is_key_old() {
+    [[ ${1%%-*} -lt "$OLDER_THAN_DATE" ]]
 }
 
 # Improved formatting output
@@ -86,16 +102,43 @@ print_user() {
 print_key() {
     local key=$1
     local date=$2
-    if $DENOTE_OLD && [[ ${date%%-*} -lt "$OLDER_THAN_DATE" ]]; then
-        if $PRETTY; then
-            echo "${SPACING}${SPACING}$key: ${RED}$date${NC}"
-        else
-            echo "${SPACING}${SPACING}$key: $date $RAW_OLD_SYMBOL"
+    local local_status=$3
+
+    if $PRETTY; then
+        # Determine colors
+        local key_color=""
+        if [[ $DISABLE_STATUS_FLAGS == false ]]; then
+            if [[ "$local_status" == "Inactive" ]]; then
+                key_color=$YELLOW
+            else
+                key_color=$GREEN
+            fi
         fi
-    elif [[ -n $date ]]; then
-        echo "${SPACING}${SPACING}$key: $date"
+
+        local date_color=""
+        if [[ $DISABLE_AGE_FLAGS == false ]]; then
+                date_color=$RED
+        fi
+
+        if $DENOTE_OLD && is_key_old "$date"; then
+            # Old Pretty
+            echo "${SPACING}${SPACING}${key_color}$key${NC}: ${date_color}$date${NC}"  # Show in red
+        else
+            # Current Pretty
+            echo "${SPACING}${SPACING}${key_color}$key${NC}: $date"
+        fi
     else
-        echo "${SPACING}${SPACING}$key: No date available"
+        # Determine if age flag or status flag should be disabled
+        local age_flag=$([[ $DISABLE_AGE_FLAGS == false ]] && echo " $RAW_OLD_SYMBOL" || echo "")
+        local status_flag=$([[ $DISABLE_STATUS_FLAGS == false && "$local_status" == "Inactive" ]] && echo " $RAW_INACTIVE_SYMBOL" || echo "")
+
+        if $DENOTE_OLD && is_key_old "$date"; then
+            # Old Raw
+            echo "${SPACING}${SPACING}$key: $date$status_flag$age_flag"
+        else
+            # Current Raw
+            echo "${SPACING}${SPACING}$key: $date$status_flag"
+        fi
     fi
 }
 
@@ -114,21 +157,31 @@ for PROF in "${OUR_AWS_PROFILES[@]}"; do
 
     # Get all keys for each user in the list
     for USER in "${USER_NAMES[@]}"; do
-        KEYS_LIST=( $(get_access_keys "$USER" "$PROF") )
+        # Get both access key ID and status
+        KEYS_LIST=$(get_access_keys "$USER" "$PROF")
 
-        if [[ ${#KEYS_LIST[@]} -gt 0 || $IGNORE_USERS_WITHOUT_KEYS == false ]]; then
+        if [[ -n "$KEYS_LIST" ]] || [[ $IGNORE_USERS_WITHOUT_KEYS == false ]]; then
             print_user "$USER"
-            if [[ -z "$KEYS_LIST" ]]; then
-                echo "${SPACING}${SPACING}None"
-            fi
         fi
 
-        for KEY in "${KEYS_LIST[@]}"; do
+        if [[ -z "$KEYS_LIST" ]]; then
+            if [[ $IGNORE_USERS_WITHOUT_KEYS == false ]]; then
+                echo "${SPACING}${SPACING}None"
+            fi
+            continue
+        fi
+
+        # Process each key and its status
+        while IFS=$'\t' read -r KEY local_status; do
+            if $IGNORE_INACTIVE_KEYS && [[ "$local_status" == "Inactive" ]]; then
+                continue
+            fi
+
             LAST_USED_DATE=$(get_last_used_date "$KEY" "$PROF")
             FORMATTED_DATE=${LAST_USED_DATE:-"Never Used"}
 
-            print_key "$KEY" "$FORMATTED_DATE"
-        done
+            print_key "$KEY" "$FORMATTED_DATE" "$local_status"
+        done <<< "$KEYS_LIST"
     done
 
     $PRETTY && echo  # Blank line between profiles
